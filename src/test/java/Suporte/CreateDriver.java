a4b28187ed4d640829afbb966c152d3cb9c2eacc
Felipe Rossi <91573168+felipe-rossi@users.noreply.github.com>
2023-05-28 14:49:34 -0300

    Ajustes driver para linux3

--- src/test/java/Suporte/CreateDriver.java
+++ src/test/java/Suporte/CreateDriver.java
@@ -19,7 +19,7 @@ public class CreateDriver {
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--no-sandbox");
 
-        System.setProperty("webdriver.chrome.driver","/home/frossi/Documentos/chromedriver");
+        System.setProperty("webdriver.chrome.driver","~/Documentos/chromedriver");
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
