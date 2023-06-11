e9335a3abd305c99b6ce1cfd22a8f1b0438fb210
Felipe Rossi <91573168+felipe-rossi@users.noreply.github.com>
2023-06-11 11:02:24 -0300

    Testar email

--- src/test/java/tests/T001KnivesTest.java
+++ src/test/java/tests/T001KnivesTest.java
@@ -30,7 +30,7 @@ public class T001KnivesTest {
     public void validarPrecoDasFacas(){
             boolean comprarSkin = new HomeSteamPO(driver)
             .clicarNaOpcaoMercadoComunidade()
-            .escolherTipoDeProdutoQueVaiSerPesquisa("tag_730_Type_CSGO_Type_Knife")
+            .escolherTipoDeProdutoQueVaiSerPesquisa("tag_weapon_ak47") //tag_weapon_ak47 tag_730_Type_CSGO_Type_Knife
             .ordernarPeloMenorPreco()
             .validarValorDaSkin();
         Assert.assertFalse(comprarSkin);
